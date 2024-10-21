79d1e106793d139c5aadabf04e46f1a4cde4c485
weitsung50110 <a1233z51ab10@gmail.com>
2024-10-21 14:52:54 +0800

    course and cursor

--- js/script.js
+++ js/script.js
@@ -24,18 +24,17 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('mousemove', function(e) {
   const cursorEffect = document.getElementById('cursor-effect');
   const scrollbarWidth = 12; // 根據你的滾動條寬度設置
-  const proximityToBottom = 50; // 接近底部的距離閾值
+  const proximityToBottom = 0; // 接近底部的距離閾值
 
   // 檢查滑鼠是否在滾動條範圍內
   const isOverScrollbar = (e.pageX > window.innerWidth - scrollbarWidth - 50);
-  // 檢查滑鼠是否接近網頁底部
-  const isNearBottom = (window.innerHeight - e.pageY < proximityToBottom);
 
-  if (isOverScrollbar || isNearBottom) {
+  console.log(window.innerHeight - e.pageY)
+  if (isOverScrollbar) {
     cursorEffect.style.opacity = 0; // 隱藏特效
   } else {
-    cursorEffect.style.left = (e.pageX + 3) + 'px'; // 向右偏移 3 像素
-    cursorEffect.style.top = (e.pageY + 10) + 'px'; // 向下偏移 10 像素
+    cursorEffect.style.left = (e.pageX -25) + 'px'; // 向右偏移 3 像素
+    cursorEffect.style.top = (e.pageY -35) + 'px'; // 向下偏移 10 像素
     cursorEffect.style.opacity = 1; // 顯示特效
   }
 });
